13036482e52276235df18487bd85aa20b0a5b5f2
ethan <121973621+simuIates@users.noreply.github.com>
2023-12-07 20:34:26 +0000

    Delete assets directory

--- assets/javascript/audio.js
+++
@@ -1,7 +0,0 @@
-var button = document.getElementById('enter');
-// var audio = new Audio('https://cdn.discordapp.com/attachments/1160879486208852038/1180228873212928050/blameSpotify.mp3');
-audio.volume = .100;
-button.addEventListener('click', function() {
-  audio.play();
-  button.style.display = 'none';
-});
